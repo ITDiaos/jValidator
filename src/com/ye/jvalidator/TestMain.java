1b9187fe47b53f970370ff34e3570d3b6ab51f27
yejg <yejg1212@gmail.com>
2013-08-28 22:47:45 +0800

    test

--- src/com/ye/jvalidator/TestMain.java
+++ src/com/ye/jvalidator/TestMain.java
@@ -3,7 +3,6 @@ package com.ye.jvalidator;
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		System.out.println("Hello,this is my test!");
 	}
 }
